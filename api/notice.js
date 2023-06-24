9d6bfe0bce87b9fb20634968917a61bddfb75136
Sujan Ghimire <ghimire.amrit147@gmail.com>
2023-06-24 16:36:55 +0545

    notice api

---
+++ api/notice.js
@@ -0,0 +1,65 @@
+export const notice = [
+  {
+    id: 1,
+    title: "First Notice",
+    image: "",
+    text: "hello world",
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 2,
+    title: "Second Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 3,
+    title: "Third Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 4,
+    title: "Fourth Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 5,
+    title: "Fifth Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 6,
+    title: "Sixth Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 7,
+    title: "Seventh Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+  {
+    id: 8,
+    title: "Eighth Notice",
+    image: "",
+    text: "hello world",
+
+    createdAt: "2023-06-24T03:29:38.393+00:00",
+  },
+];
--- pages/notice.js
+++ pages/notice.js
@@ -1,16 +1,14 @@
+import { notice } from "@/api/notice";
 import RecentNotice from "@/components/Notice/RecentNotice";
 import SingleNotice from "@/components/Notice/SingleNotice";
 import React from "react";
 
-const Notice = ({ notice }) => {
-  console.log(notice);
+const Notice = () => {
   return (
     <div className="flex justify-center w-[100vw] ">
       <div className="container md:w-[60%] px-5 py-4">
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-          {notice.map((item) => (
-            <SingleNotice item={item} />
-          ))}
+          {notice && notice.map((item) => <SingleNotice item={item} />)}
         </div>
       </div>
       <div className="container hidden md:flex md:w-[20%]  px-5 py-4">
@@ -20,13 +18,13 @@ const Notice = ({ notice }) => {
   );
 };
 
-export const getServerSideProps = async () => {
-  const result = await fetch("http://localhost:8000/notice");
-  const notice = await result.json();
+// export const getServerSideProps = async () => {
+//   const result = await fetch("http://localhost:8000/notice");
+//   const notice = await result.json();
 
-  return {
-    props: { notice },
-  };
-};
+//   return {
+//     props: { notice },
+//   };
+// };
 
 export default Notice;
