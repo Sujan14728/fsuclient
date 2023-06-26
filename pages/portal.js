13e7be5f9f5b9c4ac49e68da35716091c4b22071
Sujan Ghimire <ghimire.amrit147@gmail.com>
2023-06-26 21:49:54 +0545

    portal changed some code

--- pages/portal.js
+++ pages/portal.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react"
-import Login from "./login"
-import Form from "@/components/Portal/Form"
+import React, { useState } from "react";
+import Login from "./login";
+import Form from "@/components/Portal/Form";
 
 const Portal = () => {
-  const [loggedin, setLoggedin] = useState(true)
+  const [loggedin, setLoggedin] = useState(false);
   return (
     <div className="container mx-auto p-5">
       {loggedin ? <Form /> : <Login />}
     </div>
-  )
-}
+  );
+};
 
-export default Portal
+export default Portal;
