38ccc57ed72dbc8fcf1c331a386fb43566352707
Sujan Ghimire <ghimire.amrit147@gmail.com>
2023-06-30 20:45:49 +0545

    .....home page

--- components/Home/First.js
+++ components/Home/First.js
@@ -25,21 +25,34 @@ const First = () => {
           />
         </motion.div>
         <motion.div
-          className="flex flex-col md:w-[50%] mt-10 h-max"
+          className="flex flex-col items-center md:w-[50%] mt-10 h-max"
           initial={{ x: 100, y: -100 }}
           whileInView={{ x: 0, y: 0, transition: { duration: 1 } }}
         >
           {/* bg-gradient-to-r from-[#48408d] to-[#108cf1dc] */}
-          <h1 className="text-2xl md:text-4xl uppercase font-extrabold text-white bg-fifth-color rounded w-max px-5 py-3 font-[Montserrat]">
-            Free Student Union
-          </h1>
-          {/* <p className="uppercase px-2 text-base md:text-xl text-[#777676] mb-5 mt-2 font-[Poppins]">
-            ioe purwanchal campus
-          </p> */}
-          <p className="border-0 mb-3 border-background-color font-[Montserrat] text-[2rem] font-bold text-[#5b50b8] ">
+          <span className="text-[#F0885E] text-lg font-sans font-semibold mb-2 ">
             Empowering students. Advocating for change. Creating a better
             future.
+          </span>
+          <h1 className="text-[3rem] md:text-[3rem] uppercase font-extrabold text-[#494848] rounded w-max py-3 font-[Montserrat] leading-3 ">
+            Free Student Union
+          </h1>
+          <p className="uppercase text-base md:text-xl text-[#777676] mb-5 mt-2 font-[Poppins]">
+            ioe purwanchal campus
           </p>
+          {/* <hr className="h-2 text-black border-black bg-black " /> */}
+          <div className="w-[100%] flex flex-col border-0 mb-3 border-background-color font-[Montserrat] text-[1.2rem] font-bold text-[#474270] ">
+            <span className="mb-4 text-[1.2rem] text-justify">
+              Join us on this journey of empowerment and change. Explore our
+              website to learn more about our mission, initiatives, and events.
+              Get involved, raise your voice, and be part of the movement that
+              is shaping the future of education. Together, we can make a
+              difference.
+            </span>
+            <span className="text-[1.4rem] ">
+              Welcome to FSU. Welcome to a brighter tomorrow.
+            </span>
+          </div>
           <Link
             href="#about"
             className="bg-background-color opacity-80 hover:opacity-100 text-white px-10 mt-3 py-2 rounded-lg text-center w-max font-semibold text-lg font-[Poppins]"
