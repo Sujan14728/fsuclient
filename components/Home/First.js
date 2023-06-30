f6c68a30af287ff64bf6e8e4256105132446c9c9
Sujan Ghimire <ghimire.amrit147@gmail.com>
2023-06-30 10:14:36 +0545

    third changes

--- components/Home/First.js
+++ components/Home/First.js
@@ -1,18 +1,20 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
-import Link from "next/link"
-import { motion } from "framer-motion"
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const First = () => {
   return (
     <motion.div
-      className={`font-[Helvetica] w-[100%] h-[90vh] flex justify-center mt-10`}>
+      className={`font-[Helvetica] w-[100%] h-[90vh] flex justify-center mt-10`}
+    >
       <div className="w-[80%] flex flex-col xl:flex-row items-center ">
         <motion.div
           className="md:w-[60%]"
           initial={{ x: -100, y: -100 }}
-          whileInView={{ x: 0, y: 0, transition: { duration: 1 } }}>
+          whileInView={{ x: 0, y: 0, transition: { duration: 1 } }}
+        >
           <Image
             src="/undraw_showing_support_re_5f2v.svg"
             alt="FSU"
@@ -25,7 +27,8 @@ const First = () => {
         <motion.div
           className="flex flex-col md:w-[50%] mt-10 h-max"
           initial={{ x: 100, y: -100 }}
-          whileInView={{ x: 0, y: 0, transition: { duration: 1 } }}>
+          whileInView={{ x: 0, y: 0, transition: { duration: 1 } }}
+        >
           <h1 className="text-2xl md:text-4xl uppercase font-extrabold text-white bg-gradient-to-r from-background-color to-secondary-color rounded w-max px-5 py-3 font-[Montserrat] mb-3">
             Free Student Union
           </h1>
@@ -42,13 +45,14 @@ const First = () => {
           </p>
           <Link
             href="#about"
-            className="bg-background-color opacity-80 hover:opacity-100 text-white px-10 mt-3 py-2 rounded-lg text-center w-max font-semibold text-lg font-[Poppins]">
+            className="bg-background-color opacity-80 hover:opacity-100 text-white px-10 mt-3 py-2 rounded-lg text-center w-max font-semibold text-lg font-[Poppins]"
+          >
             About Us
           </Link>
         </motion.div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default First
+export default First;
